d98528b339919adbb9ef9795bb4f8b486edddda3
llangtonll <159337006+llangtonll@users.noreply.github.com>
2024-03-08 14:09:50 -0800

    Update BoidClass.py

--- BoidClass.py
+++ BoidClass.py
@@ -20,16 +20,28 @@ class Boid:
         self.birb = cone(pos = self.position, axis = vec(1, 0, 0), make_trail = makeTrails)
 
     def rule1(self,flock,numBirds):
-            pcj = 0
-            for birb in flock:
-                if birb == self.birb:
-                    pass
-                else:
-                    pcj = pcj + birb.pos
+        flock = flock.members
+        birb = Boid.birb
+        pcj = 0
+        for birb in flock:
+            if birb == self.birb:
+                pass
+            else:
+                pcj = pcj + birb.pos
     
-            pcj = pcj/(numBirds - 1)
-            v1 = (pcj - self.birb.pos)/100
-            return v1
+        pcj = pcj/(numBirds - 1)
+        v1 = (pcj - self.birb.pos)/100
+        return v1
+
+    def rule2(self):
+        flock = flock.members
+        birb = Boid.birb
+        c = vector(0,0,0)
+        for birb in flock:
+            if birb == self.birb:
+                pass
+            else:
+                pcj = pcj + birb.pos
 
 class Flock:
 
