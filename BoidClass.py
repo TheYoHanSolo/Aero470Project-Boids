ef3d727bc442ffee192ffc62cfdd014728fe2a7a
TheYoHanSolo <johan@govaars.us>
2024-03-08 13:50:19 -0800

    Made Boids a cone

--- BoidClass.py
+++ BoidClass.py
@@ -8,13 +8,16 @@ import math as m
 
 class Boid:
 
-    def __init__(self, simulationArea = [ [0, 1], [0, 1], [0, 1] ]):
+    def __init__(self, simulationArea = [ [0, 10], [0, 10], [0, 10] ], makeTrails = False):
         
         x = rand.uniform(simulationArea[0][0], simulationArea[0][1])
         y = rand.uniform(simulationArea[1][0], simulationArea[1][1])
         z = rand.uniform(simulationArea[2][0], simulationArea[2][1])
 
         self.position = vec(x, y, z)
+        self.birb = cone(pos = self.position, axis = vec(1, 0, 0), make_trail = makeTrails)
+
+
 
     def Rule1(self):
         pass
