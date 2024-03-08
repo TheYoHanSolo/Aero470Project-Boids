db6aca6d0dfeb5c301ad670b3df0bd1bb92173fa
TheYoHanSolo <johan@govaars.us>
2024-03-08 13:23:20 -0800

    Boids Class

---
+++ Boids.py
@@ -0,0 +1,14 @@
+from vpython import *
+
+import numpy as np
+import math as m
+import random as rand
+
+
+class Boid:
+
+    def __init__(self):
+
+        self.position = vec(0, 0, 0)
+
+        pass
