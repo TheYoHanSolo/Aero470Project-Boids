82bb3202af8e558e7e62845696cbf2b266d22694
TheYoHanSolo <johan@govaars.us>
2024-03-09 15:40:58 -0800

    Hawk Implemented

--- BoidClass.py
+++ BoidClass.py
@@ -67,6 +67,12 @@ class Boid:
         pvj = pvj/(N-1)
         v3 = (pvj - self.velocity)/16
         return v3
+    
+    def rule4(self, hawk):
+
+        v4 = -(hawk.velocity)*0.005
+
+        return v4
 
     def BoundPosition(self,xmin,xmax,ymin,ymax,zmin,zmax):
         if self.birb.pos.x < xmin:
@@ -110,9 +116,10 @@ class Flock:
 
         self.members = [Boid() for i in range(nBoids)]
         self.nBoids = nBoids
+        self.vLim = 0
 
 
-    def moveAllBoids(self, limits):
+    def moveAllBoids(self, limits, hawk):
         xmin = limits[0]
         xmax = limits[1]
         ymin = limits[2]
@@ -120,6 +127,7 @@ class Flock:
         zmin = limits[4]
         zmax = limits[5]
         vlim = limits[6]
+        self.vLim = vlim
 
         boid: Boid
         for boid in self.members:
@@ -127,7 +135,8 @@ class Flock:
             v1 = boid.rule1(self)
             v2 = boid.rule2(self)
             v3 = boid.rule3(self)
-            boid.velocity = boid.velocity + v1 + v2 + v3
+            v4 = boid.rule4(hawk)
+            boid.velocity = boid.velocity + v1 + v2 + v3 + v4
             boid.LimitSpeed(vlim)            
             
             boid.position = boid.position + boid.velocity
@@ -137,4 +146,84 @@ class Flock:
 
 
 
-    
+class Hawk:
+
+    def __init__(self, simulationArea = [ [0, 100], [0, 100], [0, 100] ], makeTrails = False, maxSpeed = 0.1, killDistance = 10):
+        
+        x = rand.uniform(simulationArea[0][0], simulationArea[0][1])
+        y = rand.uniform(simulationArea[1][0], simulationArea[1][1])
+        z = rand.uniform(simulationArea[2][0], simulationArea[2][1])
+
+        self.position = vec(x, y, z)
+        self.velocity = vec(0, 0, 0)
+        self.oob = False
+        self.maxSpeed = maxSpeed
+        self.followingFlag = False
+        self.targetedBoid = 0
+        self.killDistance = killDistance
+
+        self.hawk = cone(pos = self.position, axis = vec(-1, 0, 0), make_trail = makeTrails)
+        self.hawk.color = color.red
+
+
+    def findNearestBoid(self, flock):
+
+        boid: Boid
+        vect: vec
+        relDist = []
+        dist = []
+
+        if len(flock.members) == 0:
+
+            self.velocity = vec(0, 0, 0)
+
+        else:
+
+
+            for boid in flock.members:
+
+                relDist.append(boid.position - self.position)
+
+
+            for vect in relDist:
+
+                dist.append(vect.mag)
+
+            minDist = dist.index(min(dist))
+
+            #relVel = (relDist[minDist].norm())*self.maxSpeed
+            self.followingFlag = True
+            #self.velocity = relVel
+            self.targetedBoid = minDist
+
+            #print('Test')
+
+    def huntTargetedBoid(self, flock):
+
+        relDist = flock.members[self.targetedBoid].position - self.position
+
+        relVel = (relDist.norm())*self.maxSpeed
+
+        if relDist.mag < self.killDistance:
+
+            flock.members[self.targetedBoid].birb.color = color.magenta
+            flock.members.remove(flock.members[self.targetedBoid])
+            self.followingFlag = False
+            self.targetedBoid = 0
+
+        else:
+
+            self.velocity = self.velocity + relVel
+            self.LimitSpeed(flock.vLim)
+            self.position = self.position + self.velocity
+            self.hawk.pos = self.position
+            self.hawk.axis = self.velocity.norm()
+
+
+    def LimitSpeed(self,vlim):
+
+        hawkVLim = vlim*1.1
+        if self.velocity.mag > hawkVLim:
+            self.velocity = (self.velocity/self.velocity.mag)*hawkVLim
+        
+
--- BoidsScript.py
+++ BoidsScript.py
@@ -12,8 +12,18 @@ import BoidClass as Boid
 flock = Boid.Flock(nBoids = 30)
 limits = [-100,100,-100,100,-100,100, 0.8]
 
+hawk1 = Boid.Hawk(killDistance=5)
+
 while 1:
 
     rate(100)
-    flock.moveAllBoids(limits)
+
+    if hawk1.followingFlag == False:
+        hawk1.findNearestBoid(flock)
+
+    else:
+        hawk1.huntTargetedBoid(flock)
+
+    flock.moveAllBoids(limits, hawk1)
+
     #scene.camera.pos = flock.members[0].birb.pos
